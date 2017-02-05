b6bb95024af9536be64c24050c39357e477f3642
lizheng <li396858359@163.com>
2017-02-05 11:20:01 +0800

    不自定义textview也可以实现一个简单的Scroller滚动

--- app/src/main/java/com/hkllzh/scrollerdemo/MainActivity.java
+++ app/src/main/java/com/hkllzh/scrollerdemo/MainActivity.java
@@ -3,23 +3,31 @@ package com.hkllzh.scrollerdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Scroller;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private CustomTextView tvTest;
+    private TextView tvTest;
+    private Scroller mScroller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvTest = (CustomTextView) findViewById(R.id.tvTest);
+        tvTest = (TextView) findViewById(R.id.tvTest);
     }
 
     public void onTestScroller(View view) {
+        if (null == mScroller) {
+            mScroller = new Scroller(this);
+            tvTest.setScroller(mScroller);
+        }
         Toast.makeText(this, "testScroller", Toast.LENGTH_SHORT).show();
-        tvTest.testScroller();
+        mScroller.startScroll(tvTest.getScrollX(), tvTest.getScrollY(), -100, -100, 3000);
+        tvTest.invalidate();
     }
 }
